739c51c7fb87a9bc5cab467b067dc001156c5a9b
Ashiqur Rahman <s.m.rahman6666@gmail.com>
2023-06-15 02:21:43 +0600

    Added SingleUser

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 function SingleUser() {
   const { id } = useParams();
-  const [individualEmp, setIndividualEmp] = useState("");
+  const [individualEmp, setIndividualEmp] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,13 +12,27 @@ function SingleUser() {
         `http://59.152.62.177:8085/api/Employee/IndividualEmployeeData/${id}`
       )
       .then((response) => {
-        setIndividualEmp(response);
+        setIndividualEmp(response.data.readEmployeeData);
+        // console.log(response.data.readEmployeeData);
       });
   }, [id]);
 
-  console.log(individualEmp);
-
-  return <div>{JSON.stringify(individualEmp)}</div>;
+  return (
+    <div className="mx-auto max-w-screen-sm border-2 my-20 p-8">
+      {individualEmp.map((user) => (
+        <div key={user.id}>
+          <p>User Id: {user.empID}</p>
+          <p>First Name: {user.firstName}</p>
+          <p>Last Name: {user.lastName}</p>
+          <p>Employee Type: {user.employeeType}</p>
+          <p>Division Id: {user.divisionId}</p>
+          <p>District Id: {user.districeID}</p>
+          <p>Division: {user.disvision}</p>
+          <p>District: {user.district}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default SingleUser;
